ce67f649fa516f50a342b920f1f8c3a4e2a00a7e
rinkurinku50 <34372442+rinkurinku50@users.noreply.github.com>
2023-03-11 21:57:20 +0530

    Update chat_page.dart

--- chat_page.dart
+++ chat_page.dart
@@ -10,15 +10,18 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   List<Message> messages = [
-    Message(id: 0, received: false, text: "Hey There !", timestamp: '4:30 PM'),
+    Message(id: 0, received: false, text: " Hi Ben, it's great to finally meet you in person.", timestamp: '4:30 PM'),
     Message(
-        id: 0,
-        received: false,
-        text: "I Have been reading a lot books",
+        id: 1,
+        received: true,
+        text: "Yes, same here Alex. I've heard a lot about your company and the work you do",
         timestamp: '4:31 PM'),
     Message(
-        id: 0, received: true, text: "That's great !", timestamp: '4:32 PM'),
-    Message(id: 0, received: true, text: "Sounds Good", timestamp: '4:33 PM'),
+        id: 2, received: false, text: "Thank you. Likewise, I've been following your company's progress and I'm impressed with your growth.", timestamp: '4:32 PM'),
+    Message(id: 3, received: true, text: "Thanks, we've been working hard to expand our reach. So, what brings you here today?", timestamp: '4:33 PM'),
+    Message(id: 4, received: false, text: "I wanted to discuss the possibility of a collaboration between our two companies. I think there are some areas where we can work together to create something truly innovative.", timestamp: '4:33 PM'),
+    Message(id: 5, received: true, text: " I agree, I've been thinking the same thing. Can you tell me more about your vision?", timestamp: '4:33 PM'),
+    Message(id: 6, received: false, text: "Sure. I think we could combine our expertise in software development and data analysis to create a new product that would be highly valuable to our customers. We could each bring our own strengths to the table and work together to create something truly unique.", timestamp: '4:33 PM'),
   ];
   @override
   Widget build(BuildContext context) {
@@ -77,7 +80,7 @@ class _ChatPageState extends State<ChatPage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
                     Text(
-                      "Sarah Smith",
+                      "Alex Smith",
                       style: TextStyle(
                         fontSize: 26,
                         fontWeight: FontWeight.bold,
